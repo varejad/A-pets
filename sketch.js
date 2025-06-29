72f0d390fb976ee5b2477230ca255766d8cff8d1
varejad <junior18ademilson@gmail.com>
2025-06-28 21:55:44 -0300

    add classe e obj User com quantidade limitada de reforçadores

--- sketch.js
+++ sketch.js
@@ -10,30 +10,32 @@ let canvaHeight;
 }*/
 
 function reforcar(){
-  pyodide.runPython(`
+  if (Reflect.get(user, "reforces")>= 5){
+    pyodide.runPython(`
 agentAPet.consequence += 5
 agentAPet.mouthType = "smile"
+user.reforces -= 5
 `)
-  
+  } else{
+    console.log("usuário não possui reforçadores suficientes")
+  }   
 }
 
 async function punir(){
-  //Reflect.get(agent, 'reforcar')(-3);
   // TESTAR COMO RESOLVER A PUNIÇÃO CHEGANDO A ZERAR FATOR DE PROBABILIDADE
   pyodide.runPythonAsync(`
 agentAPet.reforcar(-3)
 agentAPet.mouthType = "sad"
-`);
+  `);
 }
 
 function enviarInstrucao() {
   const instrucao = document.getElementById("inputInstrucao").value;
   pyodide.runPython(`agentAPet.antecedente_atual = ("${instrucao}",)`);
-
   document.getElementById("labelInputInstrucao").textContent = `Instrução atual: ${instrucao}`
 }
 
-function criarAPet() {
+function criarAPeteUser() {
   const corCorpo = document.getElementById('cor-corpo').value;
   const formaCorpo = document.getElementById('forma-corpo').value;
   const corOlhos = document.getElementById('cor-olhos').value;
@@ -52,9 +54,11 @@ function criarAPet() {
                        name="${APetName}", 
                        shape = "${formaCorpo}", 
                        eyeColor="${corOlhos}", 
-                       mouthColor="${corBoca}")`);
+                       mouthColor="${corBoca}")
+    user = User()`);
       
   agent = pyodide.globals.get("agentAPet")
+  user = pyodide.globals.get("user")
   //document.getElementById("customizacao").style.display = "none";   // esconde a div de customização
   document.getElementById("controls").style.display = "block";
 
@@ -66,38 +70,38 @@ function loopAPet() {
   pyodide.runPython("simular_em_loop()")
   
   const end = performance.now();
-  console.log(`Execução do passo: ${Math.round(end - start)} ms`);
+  //console.log(`Execução do passo: ${Math.round(end - start)} ms`);
 
   setTimeout(loopAPet, 20);
 }
 
-async function updateAgentsFromPyodide() {
-  //const start = performance.now();
+// async function updateAgentsFromPyodide() {
+//   //const start = performance.now();
 
-  if (typeof pyodide !== "undefined") {
-    try {
-      await pyodide.runPythonAsync("simular_em_loop()")
+//   if (typeof pyodide !== "undefined") {
+//     try {
+//       await pyodide.runPythonAsync("simular_em_loop()")
 
-      // Pega a variável Python "agents" e transforma em array JavaScript (virou um array onde cada item é um array de chaves e valores)
-      let pyAgents = pyodide.globals.get("agents").toJs({dict_converter: Object});
+//       // Pega a variável Python "agents" e transforma em array JavaScript (virou um array onde cada item é um array de chaves e valores)
+//       let pyAgents = pyodide.globals.get("agents").toJs({dict_converter: Object});
 
-      // transformar o array de arrays recebido acima em um objeto JS
-      agents = pyAgents.map(arrayOfPairs => {
-      // Verificação opcional, mas boa prática, caso algum item não seja um array de pares
-      if (Array.isArray(arrayOfPairs)) {
-        return Object.fromEntries(arrayOfPairs);
-      }
-      return arrayOfPairs; // Retorna como está se não for um array (segurança)
-      });
-    } catch (e) {
-      console.error("Erro ao acessar agents do Pyodide:", e);
-    }
-  } else {console.log("pyodide ainda não iniciado")}
-  //const end = performance.now();
-  //console.log(`Execução do passo: ${Math.round(end - start)} ms`);
+//       // transformar o array de arrays recebido acima em um objeto JS
+//       agents = pyAgents.map(arrayOfPairs => {
+//       // Verificação opcional, mas boa prática, caso algum item não seja um array de pares
+//       if (Array.isArray(arrayOfPairs)) {
+//         return Object.fromEntries(arrayOfPairs);
+//       }
+//       return arrayOfPairs; // Retorna como está se não for um array (segurança)
+//       });
+//     } catch (e) {
+//       console.error("Erro ao acessar agents do Pyodide:", e);
+//     }
+//   } else {console.log("pyodide ainda não iniciado")}
+//   //const end = performance.now();
+//   //console.log(`Execução do passo: ${Math.round(end - start)} ms`);
 
-  //setTimeout(updateAgentsFromPyodide, 20);
-}
+//   //setTimeout(updateAgentsFromPyodide, 20);
+// }
 
 function setup() {
   canvaWidth = Math.min(windowWidth - 10, 600);
